7555bb4a4b95717661eefcd1001b9827bb95f84b
dengtianmin <2635703774@qq.com>
2025-05-29 11:21:17 +0000

    add test perfermance

--- operators /cuda/add.cuh
+++
@@ -1,9 +0,0 @@
-#include <cuda_runtime.h>
-#include <torch/extension.h>
-#include <torch/types.h>
-
-__global__ void add_kernel_cu_fp32(int32_t size, const float *in1,
-    const float *in2, float *out);
-
-__global__ void add_kernel_cu_f32x4(int32_t size, const float *in1,
-    const float *in2, float *out);
